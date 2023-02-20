4d33cc3206b3d09fb416870cb9e34fd14018cf59
Claire Devlin <clairebeargoldielox@icloud.com>
2023-02-20 12:40:43 -0800

    init, begin horizontal nav bar

---
+++ src/components/HorizontalNavBar.tsx
@@ -0,0 +1,22 @@
+import React from 'react';
+
+import Logo from '../images/template.png';
+
+import '../styles/HorizontalNavBar.css';
+
+function HorizontalNavBar() {
+  return (
+    <div id="horizontal-nav-bar">
+      <img id="h-nav-bar-logo" src={Logo}/>
+      <h1 id="h-nav-bar-title">Baskets &amp; Bows</h1>
+      <div id="h-nav-bar-btns-div">
+        <h1 className="h-nav-bar-btn" id="h-nav-bar-home-btn">Home</h1>
+        <h1 className="h-nav-bar-btn" id="h-nav-bar-home-btn">About us</h1>
+        <h1 className="h-nav-bar-btn" id="h-nav-bar-home-btn">Gallery</h1>
+        <h1 className="h-nav-bar-btn" id="h-nav-bar-home-btn">Contact</h1>
+      </div>
+    </div>
+  );
+}
+
+export default HorizontalNavBar;
