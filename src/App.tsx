495877eaf72c0fff64a30a13b41e4e046ce49d79
Claire Devlin <clairebeargoldielox@icloud.com>
2023-02-20 13:36:53 -0800

    vertical nav bar

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,38 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+
+import NavBarImg from './images/navbar.png';
 
 import HorizontalNavBar from './components/HorizontalNavBar';
+import VerticalNavBar from './components/VerticalNavBar';
+
+import './styles/App.css';
 
 function App() {
+
+  const [mobile, setMobile] = useState(false);
+  const [navVis, setNavVis] = useState<boolean>(false);
+
+  // set mobile
+  useEffect(() => {
+    console.log("ping");
+    let width = window.innerWidth;
+    if (width < 1000) {
+      setMobile(true);
+    } else {
+      setMobile(false);
+    }
+  }, [])
+  
   return (
     <div id="app">
-      <HorizontalNavBar/>
+      {mobile ? navVis ? <VerticalNavBar
+        navVis={navVis}
+        setNavVis={setNavVis}
+      /> : <img id="v-nav-bar-btn" src={NavBarImg} onClick={() => {
+        setNavVis((prev) => {
+          return !prev;
+        })
+      }}/> : <HorizontalNavBar/>}
     </div>
   );
 }
---
+++ src/components/VerticalNavBar.tsx
@@ -0,0 +1,30 @@
+import React, { SetStateAction } from 'react';
+
+import Logo from '../images/template.png';
+
+import '../styles/VerticalNavBar.css';
+
+interface VerticalNavBarProps {
+    navVis: boolean;
+    setNavVis: React.Dispatch<SetStateAction<(boolean)>>
+}
+
+export const VerticalNavBar: React.FC<VerticalNavBarProps> = ({navVis, setNavVis}) => {
+  return (
+    <div id="vertical-nav-bar">
+        <h1 id="v-nav-bar-x" onClick={() => {
+            setNavVis((prev) => {
+                return !prev;
+            })
+        }}>X</h1>
+      <div id="v-nav-bar-btns-div">
+        <h1 className="v-nav-bar-btn" id="v-nav-bar-home-btn">Home</h1>
+        <h1 className="v-nav-bar-btn" id="v-nav-bar-home-btn">About us</h1>
+        <h1 className="v-nav-bar-btn" id="v-nav-bar-home-btn">Gallery</h1>
+        <h1 className="v-nav-bar-btn" id="v-nav-bar-home-btn">Contact</h1>
+      </div>
+    </div>
+  );
+}
+
+export default VerticalNavBar;
