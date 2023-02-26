a5d44db9478ac84095b148763301a2703cb3f0b9
Claire Devlin <clairebeargoldielox@icloud.com>
2023-02-26 12:25:25 -0800

    fix mobile gallery img click

--- src/components/GalleryImage.tsx
+++ src/components/GalleryImage.tsx
@@ -1,5 +1,5 @@
-import React from 'react';
-
+import React, { useState, useRef } from 'react';
+import {isMobile} from 'react-device-detect';
 
 interface GalleryImageProps {
     path: string;
@@ -8,10 +8,28 @@ interface GalleryImageProps {
 }
 
 export const GalleryImage: React.FC<GalleryImageProps> = ({path, desc, k}) => {
+
+  const [show, setShow] = useState(true);
+
+  const divRef = useRef<any>();
+  const imgRef = useRef<any>();
+  const descRef = useRef<any>();
+
   return (
-    <div className="gallery-image-div">
-        <img className="gallery-img" src={path}/>
-        <p className="gallery-desc">{desc}</p>
+    <div ref={divRef} className="gallery-image-div" onClick={() => {
+      if (isMobile) {
+        setShow(prev => !prev);
+        if (show) {
+          imgRef.current.style.filter = "brightness(50%) blur(2px)";
+          descRef.current.style.visibility = "visible";
+        } else {
+          imgRef.current.style.filter = "brightness(100%) blur(0px)";
+          descRef.current.style.visibility = "hidden";
+        }
+      }
+    }}>
+        <img ref={imgRef}className="gallery-img" src={path}/>
+        <p ref={descRef} className="gallery-desc">{desc}</p>
     </div>
   );
 }
