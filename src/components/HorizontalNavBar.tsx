ee9fc2ad753105a0712e3c7c28ee30a80e4be4cf
Claire Devlin <clairebeargoldielox@icloud.com>
2023-02-27 17:00:18 -0800

    mld

--- src/components/HorizontalNavBar.tsx
+++ src/components/HorizontalNavBar.tsx
@@ -10,11 +10,12 @@ function HorizontalNavBar() {
       <img id="h-nav-bar-logo" src={Logo}/>
       <h1 id="h-nav-bar-title">Baskets &amp; Bows</h1>
       <div id="h-nav-bar-btns-div">
-        <a href="#home-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">Home</a>
-        <a href="#about-us-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">About Me</a>
-        <a href="#gallery-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">Gallery</a>
-        <a href="#how-it-works-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">How it Works</a>
         <a href="#contact-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">Contact</a>
+        <a href="#how-it-works-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">How it Works</a>
+        <a href="#gallery-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">Gallery</a>
+        <a href="#about-us-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">About Me</a>
+        <a href="#home-section" className="h-nav-bar-btn" id="h-nav-bar-home-btn">Home</a>
+        
       </div>
     </div>
   );
