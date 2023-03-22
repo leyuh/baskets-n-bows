d6466f6f3c47eee511a096056340a5238d9c3682
Claire Devlin <clairebeargoldielox@icloud.com>
2023-03-22 16:39:35 -0700

    update gallery imgs

--- src/galleryModule.tsx
+++ src/galleryModule.tsx
@@ -1,17 +1,17 @@
 import a from './images/gallery/product-1.jpeg';
 import b from './images/gallery/product-2.jpeg';
 import c from './images/gallery/product-3.jpeg';
-import d from './images/gallery/product-4.jpeg';
+import d from './images/gallery/product-20.jpeg';
 import e from './images/gallery/product-5.jpeg';
 import f from './images/gallery/product-6.jpeg';
 
-import h from './images/gallery/product-8.jpeg';
-import i from './images/gallery/product-9.jpeg';
+import h from './images/gallery/product-21.jpeg';
+import i from './images/gallery/product-22.jpeg';
 
 import k from './images/gallery/product-11.jpeg';
 import l from './images/gallery/product-12.jpeg';
-import m from './images/gallery/product-13.jpeg';
-import n from './images/gallery/product-14.jpeg';
+import m from './images/gallery/product-23.jpeg';
+
 import o from './images/gallery/product-15.jpeg';
 import p from './images/gallery/product-16.jpeg';
 import q from './images/gallery/product-17.jpeg';
@@ -22,21 +22,20 @@ import s from './images/gallery/product-19.jpeg';
 const GALLERY = [
     [c, "For The Gentleman"],
     [a, "Kountry Kutie"],
-    [i, "Succulent Sweetie"],
-    [d, "Gift For Mama Bear"],
     [f, "Coffee Lover"],
     [e, "Diamonds & Chanel"],
     [s, "Cowboy Take Me Away"],
     [b, "Little Black Basket"],
     [r, "Royal Tea Party"],
-    [h, "Sunny Pick-Me-Up"],
     [k, "Hand Poured Soy Candles"],
     [l, "Valentine Lovely"],
-    [m, "Valentine Lovely Times Three"],
-    [n, "Disney Baby Girl"],
     [o, "Old Spice is Nice"],
     [p, "Magical Harry Potter"],
     [q, "Wine Country"],
+    [i, "Tween Queen"],
+    [h, "St. Patty's Special"],
+    [d, "Earth Toned Beauty"],
+    [m, "Easter Baby"],
 ]
 
 export default GALLERY;
